b1f63e1037acc492b4b649838fd16987e49601ee
asifhossain <asifhossain976@gmail.com>
2021-07-04 21:36:57 +0600

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -222,6 +222,108 @@ Route::post('/manager/bug/add','ManagerController@addBugReport');
 
 
     Route::get('/index/logout', "clientController@logout")->name("client.logout");
+    //--------------------------Money Exchange Officer-------------------------
+
+Route::get('/login/meo', "LoginMeoController@index");
+Route::post('/login/meo', "LoginMeoController@verify");
+Route::get('/logout/meo', "LogoutController@logout");
+
+
+// Route::group(['middleware'=>['sessionMeo']],function()
+// {
+
+Route::group(['middleware'=>['meo']],function()
+{
+    Route::get('/homeMeo', "HomeMeoController@index");
+    Route::get('/requestView', "reqController@index");
+    Route::get('/addRequest', "reqController@show");
+    Route::post('/addRequest', "reqController@insert");
+
+    Route::get('/editRequest', "reqController@editview");
+    Route::get('/editRequest/{id}', "reqController@edit");
+    Route::post('/editRequest/{id}', "reqController@update");
+
+
+    Route::get('/viewRequest', "reqController@detailsView");
+    Route::get('/detailsRequest/{id}', "reqController@details");
+
+
+    Route::get('/deleteRequest', "reqController@deleteview");
+    Route::get('/deleteRequest/{id}', "reqController@delete");
+    Route::post('/deleteRequest/{id}', "reqController@destroy");
+
+   
+
+    Route::get('/viewClient', "clientControl@view");
+    
+
+    Route::get('/request/moneyExchange', "ExchangeController@view");
+
+
+    //client_search
+
+    Route::get('/client_search', 'ClientSearch@index');
+    Route::get('/live_search/action', 'ClientSearch@action')->name('client_search.action');
+
+    //pdf
+
+    Route::get('/client_pdf', 'ClientPDFController@index');
+
+    Route::get('/client_pdf/pdf', 'ClientPDFController@pdf');
+
+
+    //currency Convert
+
+    Route::get('/currencyConvert', 'currencyController@index');
+    Route::post('/currencyConvert', 'currencyController@convert');
+
+
+    //Contact Manager with JSON
+
+
+    Route::get('/contact/store', 'ContactController@index');
+    Route::post('/contact/store', 'ContactController@store');
+
+
+
+    //transactions
+
+
+    Route::get('/transaction_pdf', 'TransactionPDFController@index');
+
+    Route::get('/transaction_pdf/pdf', 'TransactionPDFController@pdf');
+
+
+
+    // Review Problem
+
+    Route::get('/reviewView', "reviewController@index");
+    Route::get('/addReview', "reviewController@show");
+    Route::post('/addReview', "reviewController@insert");
+
+    Route::get('/editReview', "reviewController@editview");
+    Route::get('/editReview/{id}', "reviewController@edit");
+    Route::post('/editReview/{id}', "reviewController@update");
+
+    
+    Route::get('/deleteReview', "reviewController@deleteview");
+    Route::get('/deleteReview/{id}', "reviewController@delete");
+    Route::post('/deleteReview/{id}', "reviewController@destroy");
+
+
+    //offer
+
+    Route::get('/offerView', "OfferController@index");
+
+    Route::get('/rates', "rateController@index");
+
+    //
+
+    Route::get('/contact/store', 'ContactController1@index');
+    Route::post('/contact/store', 'ContactController1@store');
+
+
+    });
 
      
     
